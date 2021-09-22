3965d3a23f46d12dfa81a2559b1f2c43b1ad58c7
Kaiying Fu <kaiying.fu@gmail.com>
2021-09-22 16:59:20 +0800

    Add Feature: Navigate Across Pages of Movies

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -2,18 +2,39 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { useMovies } from 'domains/movie/hooks/use-movies';
 import { MovieCard } from 'domains/movie/components/movie-card';
-
+import { Button } from 'components/button';
 
 export const Movies = () => {
   const { data: movies, status, page, setPage } = useMovies();
+  const PrevButton = (
+    <Button
+      disabled={page <= 1}
+      onClick={() => setPage((prevNum) => prevNum - 1)}>
+      Prev
+    </Button>
+  );
+  
+  const NextButton = (
+    <Button
+      disabled={!movies}
+      onClick={() => setPage((prevNum) => prevNum + 1)}>
+      Next
+    </Button>
+  );
   
   return (
-    <div className="grid lg:grid-cols-5 gap-10">
-      {movies && movies.map(movie =>
-        <Link to={`/movie/${movie._id}`} key={movie._id}>
-          <MovieCard movie={movie} />
-        </Link>
-      )}
-    </div>
+    <>
+      <div className="flex justify-between">
+        {PrevButton}
+        {NextButton}
+      </div>
+      <div className="grid lg:grid-cols-5 gap-10">
+        {movies && movies.map(movie =>
+          <Link to={`/movie/${movie._id}`} key={movie._id}>
+            <MovieCard movie={movie} />
+          </Link>
+        )}
+      </div>
+    </>
   );
 };
